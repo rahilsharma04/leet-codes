ddfb590a3ecaec42fbde8fa504301fdf3ca4442b
rahilsharma04 <rahilsharma1122@gmail.com>
2025-11-11 14:52:58 +0530

    Time: 0 ms (100%), Space: 8.8 MB (90.13%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,6 +1,20 @@
-class Solution {
+ class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+        int length = 0;
+        int i = s.size() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        // Count characters of the last word
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
     }
 };
